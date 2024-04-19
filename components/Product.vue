1f4725671bd128818cff6a7776fc433bd8f4ab89
legionite-scripts <samuelonwuka88@gmail.com>
2024-04-19 21:17:22 +0100

    Product component omplementation in progress

---
+++ components/Product.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="product center p-1">
+    <img src="@/assets/images/Home/product.png" alt="Item" />
+    <div>
+      <h3>Peanut Butter</h3>
+      <p>Organic, gluten-free, high protein</p>
+      <p>$20 <span>$30</span></p>
+    </div>
+
+    <div class="button">
+        <button>Quick View</button>
+        <a href="#"></a>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.product {
+  border: 1px solid #333;
+  width: 23%;
+  height: 30em;
+  background-color: #f8f9f8;
+}
+.product > div {
+  width: 100%;
+}
+.product > img {
+  width: 100%;
+  user-select: none;
+  -webkit-user-drag: none;
+  border: 1px solid #333;
+}
+</style>
--- pages/Home.vue
+++ pages/Home.vue
@@ -6,19 +6,30 @@
       <button class="pr-3 pl-3">Go</button>
     </div>
 
-    <div class="background mt-5 p-5">
-      <h1>Innovative Consumable</h1>
-      <p>
+    <!-- Background -->
+    <div class="background mt-5 mb-5 p-5">
+      <h1 class="mb-5 mt-5">
+        Innovative<br />
+        Consumable
+      </h1>
+      <p class="mb-5">
         We turn agricultural products into delicious and nutritious finished
         goods Explore Products
       </p>
-      <button class="text-center">
-        Explore Products
+      <button class="text-center p-1 pr-2 pl-2">
+        Explore Products&nbsp;
         <img src="@/assets/images/Home/arrow-narrow-right.png" alt="Right" />
       </button>
     </div>
 
-    <h1>kpk</h1>
+    <!-- Products -->
+    <div class="products center">
+      <h2 class="mb-2">Products</h2>
+
+      <div>
+        <Product/>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -29,8 +40,7 @@
   background-color: #e2dede;
   display: flex;
   flex-direction: column;
-  /* height: 100vh; */
-  width: 100vw;
+  width: 100%;
 }
 .search {
   display: flex;
@@ -50,19 +60,52 @@
   border: none;
   border-radius: 4px;
 }
+
+/* Background */
 .background {
   height: 80vh;
-
-  background: url("../assets/images/Home/bg.png") no-repeat,
-    linear-gradient(to bottom, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.7) 100%);
+  background: url("@/assets/images/Home/bg.png") no-repeat,
+    linear-gradient(to bottom, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0.8) 100%);
   background-size: cover;
-  background-blend-mode: darken !important ;
+  background-blend-mode: darken !important;
+  width: 100%;
+}
+.background > h1,
+.background > p {
+  color: #ffffff;
+}
+.background > h1 {
+  font-size: 6em;
+}
+.background > button {
+  background-color: #327a30;
+  border-radius: 4px;
+  border: none;
+  color: #ffffff;
+  font-size: 1.1em;
+  display: flex;
+  align-items: center;
+}
+/*  */
+.products {
+  border: 1px solid #333;
+}
+.products > div{
+  border: 1px solid red;
+  width: 100%;
 }
 
 /* Screens lower than 1000px */
 @media (max-width: 1000px) {
   .wrapper {
     padding: 20px;
+  }
+  .background {
+    height: 70vh;
+    padding-left: 1.5em;
+  }
+  .background > h1 {
+    font-size: 3em;
   }
 }
 </style>
